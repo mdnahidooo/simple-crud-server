c1ec86ffbacba5dbf313d29b397da1c4295f237c
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-05-07 16:44:29 +0600

    Update route on server and received data from client

--- index.js
+++ index.js
@@ -59,6 +59,29 @@ async function run() {
         })
 
 
+        app.patch('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {
+                _id: new ObjectId(id)
+            }
+            const modifiedUser = req.body;
+
+            const updatedDocument = {
+                $set: {
+                    name: modifiedUser.name,
+                    email: modifiedUser.email,
+                    role: modifiedUser.role
+                }
+            }
+
+            const result = await userCollection.updateOne(filter, updatedDocument);
+
+            res.send(result);
+
+        })
+
+
+        
         app.delete('/users/:id', async (req, res) => {
             const id = req.params.id;
             const query = {
